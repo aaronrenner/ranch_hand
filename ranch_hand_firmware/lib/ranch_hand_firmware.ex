3369310ec7f3da93fb122258932a43061cefd47e
Aaron Renner <aaron.renner@gmail.com>
2018-11-02 07:32:00 -0600

    Initial commit

---
+++ ranch_hand_firmware/lib/ranch_hand_firmware.ex
@@ -0,0 +1,18 @@
+defmodule RanchHandFimware do
+  @moduledoc """
+  Documentation for RanchHandFimware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RanchHandFimware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ranch_hand_firmware/test/ranch_hand_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule RanchHandFimwareTest do
+  use ExUnit.Case
+  doctest RanchHandFimware
+
+  test "greets the world" do
+    assert RanchHandFimware.hello() == :world
+  end
+end
